64e2cbe7d7d32721866432abc0808ce4ad54cf9b
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-24 09:12:37 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+# Anagram .new has an initialize method that takes in a word
+# Anagram #match detects no matches
+# Anagram #match detects a simple anagram
+# Anagram #match detects an anagram
+# Anagram #match detects multiple anagrams
+
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(array)
+        array.select do |w|
+        w.split("").sort == @word.split("").sort
+        end
+    end
+    
+    end
+    
+   
